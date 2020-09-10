c8bad8f254db00039b95fa9f711f24a612157759
cdfortes <cdfortes@gmail.com>
2020-09-10 17:38:42 -0100

    :rocket: eslint configuration

---
+++ src/components/Profile/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Profile = () => {
+  return (
+    <div className="Profile-wrapper">
+      <h1>Matt Foo</h1>
+      <h2>Programador Backend</h2>
+      <p>
+        Membro do Core Team da Empresa Foo, escrevo sobre o mundo backend e
+        outras coisas mais.
+      </p>
+    </div>
+  )
+}
+
+export default Profile
